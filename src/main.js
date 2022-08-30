2c89ea2bd1be9a94fa624aa93de914b99e6a72fc
linda <wd5linda5@gmail.com>
2022-08-30 10:28:30 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,100 +1,111 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as CANNON from 'cannon-es';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Player } from './Player';
-import { House } from './House';
-import { Tree } from './tree';
-import gsap from 'gsap';
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import * as CANNON from "cannon-es";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Player } from "./Player";
+import { House } from "./House";
+import { Tree } from "./tree";
+import { Cloud } from "./cloud";
+import gsap from "gsap";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 // Texture
 const textureLoader = new THREE.TextureLoader();
-const floorTexture = textureLoader.load('/images/ocean.jpg');
+// const floorTexture = textureLoader.load('/images/ocean.jpg');
 const matcapTexture = textureLoader.load("/textures/matcaps/7.png");
-floorTexture.wrapS = THREE.RepeatWrapping;
-floorTexture.wrapT = THREE.RepeatWrapping;
-floorTexture.repeat.x = 1;
-floorTexture.repeat.y = 1;
+// floorTexture.wrapS = THREE.RepeatWrapping;
+// floorTexture.wrapT = THREE.RepeatWrapping;
+// floorTexture.repeat.x = 1;
+// floorTexture.repeat.y = 1;
 
-/**
- * Fonts
- */
-const fontLoader = new FontLoader()
+//Fonts
+const fontLoader = new FontLoader();
 
-fontLoader.load(
-    '/fonts/raleway_black_regular.json',
-    (font) =>
-    {
-        // Material
-        // const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
-      const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
-      
-        // Text
-        const textGeometry = new TextGeometry(
-            'EUNJI LINDA LEE',
-            {
-                font: font,
-                size: 0.6,
-                height: 0.2,
-                curveSegments: 12,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 2
-            }
-        )
-        textGeometry.center()
+fontLoader.load("/fonts/raleway_black_regular.json", font => {
+  // Material
+  // const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 
-      const text = new THREE.Mesh(textGeometry, material)
-      text.receiveShadow = true;
-      text.castShadow = true;
-      text.position.set(0.5, 0.4, -1)
-        scene.add(text)
+  // Text
+  const textGeometry = new TextGeometry("EUNJI LINDA LEE", {
+    font: font,
+    size: 0.6,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 2,
+  });
+  textGeometry.center();
 
-        // Donuts
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+  const text = new THREE.Mesh(textGeometry, material);
+  text.receiveShadow = true;
+  text.castShadow = true;
+  text.position.set(0.5, 0.4, -1);
+  scene.add(text);
 
-        for(let i = 0; i < 100; i++)
-        {
-            const donut = new THREE.Mesh(donutGeometry, material)
-            donut.position.x = (Math.random() - 0.5) * 10
-            donut.position.y = (Math.random() - 0.5) * 10
-            donut.position.z = (Math.random() - 0.5) * 10
-            donut.rotation.x = Math.random() * Math.PI
-            donut.rotation.y = Math.random() * Math.PI
-            const scale = Math.random()
-            donut.scale.set(scale, scale, scale)
+  // const textFijiGeometry = new TextGeometry("FIJI", {
+  //   font: font,
+  //   size: 0.6,
+  //   height: 0.2,
+  //   curveSegments: 12,
+  //   bevelEnabled: true,
+  //   bevelThickness: 0.03,
+  //   bevelSize: 0.02,
+  //   bevelOffset: 0,
+  //   bevelSegments: 2,
+  // });
+  // textFijiGeometry.center();
 
-            // scene.add(donut)
-        }
-    }
-)
+  const textFiji = new THREE.Mesh(textFijiGeometry, material);
+  textFiji.receiveShadow = true;
+  textFiji.castShadow = true;
+  textFiji.position.set(0.4, 0.4, 5);
+  textFiji.rotateY(45);
+  scene.add(textFiji);
+
+  // Donuts
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64);
+
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+    donut.rotation.x = Math.random() * Math.PI;
+    donut.rotation.y = Math.random() * Math.PI;
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    // scene.add(donut)
+  }
+});
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
+const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas,
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; //makes shadow with less pixels
- 
+
 // Scene
 const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.OrthographicCamera(
-	-(window.innerWidth / window.innerHeight), // left
-	window.innerWidth / window.innerHeight, // right,
-	1, // top
-	-1, // bottom
-	-1000,
-	1000
+  -(window.innerWidth / window.innerHeight), // left
+  window.innerWidth / window.innerHeight, // right,
+  1, // top
+  -1, // bottom
+  -1000,
+  1000
 );
 
 const cameraPosition = new THREE.Vector3(1, 5, 5);
@@ -114,10 +125,10 @@ directionalLight.position.y = directionalLightOriginPosition.y;
 directionalLight.position.z = directionalLightOriginPosition.z;
 directionalLight.castShadow = true; //real light that receives on the object
 
-// mapSize 세팅으로 그림자 퀄리티 설정
+// Shadow quality Settings : by mapSize
 directionalLight.shadow.mapSize.width = 2048;
 directionalLight.shadow.mapSize.height = 2048;
-// 그림자 범위
+// Shadow Range
 directionalLight.shadow.camera.left = -100;
 directionalLight.shadow.camera.right = 100;
 directionalLight.shadow.camera.top = 100;
@@ -131,8 +142,7 @@ scene.add(directionalLight);
 
 //Cannon
 const cannonWorld = new CANNON.World();
-cannonWorld.gravity.set(0, -10, 0);
-
+cannonWorld.gravity.set(0, -9.8, 0); //gravity setup
 
 //Contact Material
 const defaultMaterial = new CANNON.Material("default");
@@ -140,14 +150,14 @@ const rubberMaterial = new CANNON.Material("rubber");
 const ironMaterial = new CANNON.Material("iron");
 
 const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
-  friction: 0.5,
+  friction: 0.1,
   restitution: 0.3,
 });
 cannonWorld.defaultContactMaterial = defaultContactMaterial;
 
 const rubberDefaultContactMaterial = new CANNON.ContactMaterial(rubberMaterial, defaultMaterial, {
   friction: 0.5,
-  restitution: 0.7,
+  restitution: 0.9,
 });
 cannonWorld.addContactMaterial(rubberDefaultContactMaterial);
 
@@ -157,17 +167,15 @@ const ironDefaultContactMaterial = new CANNON.ContactMaterial(ironMaterial, defa
 });
 cannonWorld.addContactMaterial(ironDefaultContactMaterial);
 
-
+//Cannon
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body({
   mass: 0, //if 1, it passes through plane
-  position: new CANNON.Vec3(1, 3, 0),
-  shape: floorShape
+  position: new CANNON.Vec3(0, 0, 0),
+  shape: floorShape,
 });
-floorBody.quaternion.setFromAxisAngle(
-  new CANNON.Vec3(-1.2, 0, 0),
-  Math.PI / 2
-);
+//rotate ground body by 90 degrees
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
 cannonWorld.addBody(floorBody);
 
 //Rectangle Box
@@ -178,18 +186,19 @@ const boxBody = new CANNON.Body({
   material: ironMaterial,
   shape: boxShape,
 });
-cannonWorld.addBody(boxBody);
+// cannonWorld.addBody(boxBody);
 
 //Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
-  position: new CANNON.Vec3(1, 10, 1),
+  position: new CANNON.Vec3(0, 10, 1),
   shape: sphereShape,
   material: rubberMaterial,
   // material: ironMaterial,
 });
-cannonWorld.addBody(sphereBody);
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0));
+// cannonWorld.addBody(sphereBody);
 
 // Mesh
 const meshes = [];
@@ -198,59 +207,58 @@ const floorMesh = new THREE.Mesh(
   new THREE.MeshStandardMaterial({
     // map: floorTexture,
     color: "#719fed",
+    material: defaultMaterial,
   })
 );
-floorMesh.name = 'floor';
-floorMesh.rotation.x = -Math.PI/2;
+floorMesh.name = "floor";
+floorMesh.rotation.x = -Math.PI / 2;
 floorMesh.receiveShadow = true; //shadow that shows on the surface plane
 scene.add(floorMesh);
 meshes.push(floorMesh);
 
-
-//Physics Box
-const boxGeometry = new THREE.BoxGeometry(1, 2, 1);
+//Physics Box Mesh
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.position.y = 0;
+boxMesh.position.y = 0.5;
 boxMesh.receiveShadow = true;
 boxMesh.castShadow = true;
 scene.add(boxMesh);
 
-//Physics Sphere
+//Physics Sphere Mesh
 const sphereGeometry = new THREE.SphereGeometry(0.5);
 const sphereMaterial = new THREE.MeshStandardMaterial({
   color: "seagreen",
 });
 const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphereMesh.position.y = 0;
+sphereMesh.position.y = 0.5;
 sphereMesh.castShadow = true;
 scene.add(sphereMesh);
 
-
 //Pointer Mesh
 const pointerMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(1, 1),
-	new THREE.MeshBasicMaterial({
-		color: 'crimson',
-		transparent: true,
-		opacity: 0.5
-	})
+  new THREE.PlaneGeometry(1, 1),
+  new THREE.MeshBasicMaterial({
+    color: "white",
+    transparent: true,
+    opacity: 0.5,
+  })
 );
-pointerMesh.rotation.x = -Math.PI/2;
+pointerMesh.rotation.x = -Math.PI / 2;
 pointerMesh.position.y = 0.01;
 pointerMesh.receiveShadow = true;
 scene.add(pointerMesh);
 
 const spotMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(3, 3),
-	new THREE.MeshStandardMaterial({
-		color: 'yellow',
-		transparent: true,
-		opacity: 0.5
-	})
+  new THREE.PlaneGeometry(3, 3),
+  new THREE.MeshStandardMaterial({
+    color: "black",
+    transparent: true,
+    opacity: 0.5,
+  })
 );
 spotMesh.position.set(5, 0.005, 5);
-spotMesh.rotation.x = -Math.PI/2;
+spotMesh.rotation.x = -Math.PI / 2;
 spotMesh.receiveShadow = true;
 scene.add(spotMesh);
 
@@ -268,9 +276,19 @@ const house = new House({
 const tree = new Tree({
   gltfLoader,
   scene,
-  modelSrc: "/models/tree.glb",
+  modelSrc: "/models/trees.glb",
   x: 7,
   y: 0,
+  z: 2,
+  scale: 0.01,
+});
+
+const cloud = new Cloud({
+  gltfLoader,
+  scene,
+  modelSrc: "/models/cloud.glb",
+  x: 5,
+  y: 1,
   z: 2,
   scale: 0.01,
 });
@@ -287,29 +305,53 @@ const tree = new Tree({
 
 const player = new Player({
   scene,
-	meshes,
-	gltfLoader,
-  modelSrc: '/models/best-girl.glb',
+  meshes,
+  gltfLoader,
+  modelSrc: "/models/best-girl.glb",
   scale: 0.01,
 });
+
+// const playerBody = new CANNON.Body({
+//   mass: 5,
+//   position: new CANNON.Vec3(0, 6, 0),
+//   shape: new CANNON.Box(new CANNON.Vec3(4, 0.5, 2)),
+// });
+
+// const shape = new CANNON.Cylinder(2, 1, 5, 20)
+// const body = new CANNON.Body({
+//   mass: 1,
+//   position: new CANNON.Vec3(0, 3, 0),
+//   shape: shape,
+//   material: defaultMaterial
+// })
+// world.addBody(body)
+// objectsToUpdate.push({
+//   roseAndBody: {
+//     rose: rose,
+//     body: body
+//   }
+// })
 
 const raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 let destinationPoint = new THREE.Vector3();
 let angle = 0;
-let isPressed = false; // 마우스를 누르고 있는 상태
+let isPressed = false; // Pressing on to the mouse
 
-// 그리기
+// Draw
 const clock = new THREE.Clock();
 
 function draw() {
   const delta = clock.getDelta();
-
-  // console.log(delta);
   let cannonStepTime = 1 / 60;
   if (delta < 0.01) cannonStepTime = 1 / 120;
   cannonWorld.step(cannonStepTime, delta, 3);
   // floorMesh.position.copy(floorBody.position);
+
+  //   for(const object of objectsToUpdate){
+  //     object.roseAndBody.rose.position.copy(object.roseAndBody.body.position)
+  //     object.roseAndBody.rose.quaternion.copy(object.roseAndBody.body.quaternion)
+  //  }
 
   //rectangle
   boxMesh.position.copy(boxBody.position);
@@ -331,7 +373,7 @@ function draw() {
     }
 
     if (player.moving) {
-      // 걸어가는 상태
+      // Walking
       angle = Math.atan2(destinationPoint.z - player.modelMesh.position.z, destinationPoint.x - player.modelMesh.position.x);
       player.modelMesh.position.x += Math.cos(angle) * 0.05;
       player.modelMesh.position.z += Math.sin(angle) * 0.05;
@@ -345,12 +387,12 @@ function draw() {
 
       if (Math.abs(destinationPoint.x - player.modelMesh.position.x) < 0.03 && Math.abs(destinationPoint.z - player.modelMesh.position.z) < 0.03) {
         player.moving = false;
-        console.log("멈춤");
+        console.log("Stop");
       }
 
       if (Math.abs(spotMesh.position.x - player.modelMesh.position.x) < 1.5 && Math.abs(spotMesh.position.z - player.modelMesh.position.z) < 1.5) {
         if (!house.visible) {
-          console.log("나와");
+          console.log("Show");
           house.visible = true;
           spotMesh.material.color.set("seagreen");
           gsap.to(house.modelMesh.position, {
@@ -364,7 +406,7 @@ function draw() {
           });
         }
       } else if (house.visible) {
-        console.log("들어가");
+        console.log("Hide");
         house.visible = false;
         spotMesh.material.color.set("yellow");
         gsap.to(house.modelMesh.position, {
@@ -381,7 +423,6 @@ function draw() {
       // player.actions[1].stop();
       // player.actions[0].play();
       player.actions[0].stop();
-      // wind.actions[0].play();
     }
   }
 
@@ -390,79 +431,79 @@ function draw() {
 }
 
 function checkIntersects() {
-	// raycaster.setFromCamera(mouse, camera);
+  // raycaster.setFromCamera(mouse, camera);
 
-	const intersects = raycaster.intersectObjects(meshes);
-	for (const item of intersects) {
-		if (item.object.name === 'floor') {
-			destinationPoint.x = item.point.x;
-			destinationPoint.y = 30;
-			destinationPoint.z = item.point.z;
-			player.modelMesh.lookAt(destinationPoint);
+  const intersects = raycaster.intersectObjects(meshes);
+  for (const item of intersects) {
+    if (item.object.name === "floor") {
+      destinationPoint.x = item.point.x;
+      destinationPoint.y = 30;
+      destinationPoint.z = item.point.z;
+      player.modelMesh.lookAt(destinationPoint);
 
-			// console.log(item.point)
+      // console.log(item.point)
 
-			player.moving = true;
+      player.moving = true;
 
-			pointerMesh.position.x = destinationPoint.x;
-			pointerMesh.position.z = destinationPoint.z;
-		}
-		break;
-	}
+      pointerMesh.position.x = destinationPoint.x;
+      pointerMesh.position.z = destinationPoint.z;
+    }
+    break;
+  }
 }
 
 function setSize() {
-	camera.left = -(window.innerWidth / window.innerHeight);
-	camera.right = window.innerWidth / window.innerHeight;
-	camera.top = 1;
-	camera.bottom = -1;
+  camera.left = -(window.innerWidth / window.innerHeight);
+  camera.right = window.innerWidth / window.innerHeight;
+  camera.top = 1;
+  camera.bottom = -1;
 
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 }
 
-// 이벤트
-window.addEventListener('resize', setSize);
+// Event
+window.addEventListener("resize", setSize);
 
-// 마우스 좌표를 three.js에 맞게 변환
+// Mouse Coordinates transformed according to three.js
 function calculateMousePosition(e) {
-	mouse.x = e.clientX / canvas.clientWidth * 2 - 1;
-	mouse.y = -(e.clientY / canvas.clientHeight * 2 - 1);
+  mouse.x = (e.clientX / canvas.clientWidth) * 2 - 1;
+  mouse.y = -((e.clientY / canvas.clientHeight) * 2 - 1);
 }
 
-// 변환된 마우스 좌표를 이용해 래이캐스팅
+// RayCasting by transformed Mouse coordinates
 function raycasting() {
-	raycaster.setFromCamera(mouse, camera);
-	checkIntersects();
+  raycaster.setFromCamera(mouse, camera);
+  checkIntersects();
 }
 
-// 마우스 이벤트
-canvas.addEventListener('mousedown', e => {
-	isPressed = true;
-	calculateMousePosition(e);
+// Mouse Event
+canvas.addEventListener("mousedown", e => {
+  isPressed = true;
+  calculateMousePosition(e);
 });
-canvas.addEventListener('mouseup', () => {
-	isPressed = false;
+canvas.addEventListener("mouseup", () => {
+  isPressed = false;
 });
-canvas.addEventListener('mousemove', e => {
-	if (isPressed) {
-		calculateMousePosition(e);
-	}
+canvas.addEventListener("mousemove", e => {
+  if (isPressed) {
+    calculateMousePosition(e);
+  }
 });
 
-// 터치 이벤트
-canvas.addEventListener('touchstart', e => {
-	isPressed = true;
-	calculateMousePosition(e.touches[0]);
+// Touch Event
+canvas.addEventListener("touchstart", e => {
+  isPressed = true;
+  calculateMousePosition(e.touches[0]);
 });
-canvas.addEventListener('touchend', () => {
-	isPressed = false;
+canvas.addEventListener("touchend", () => {
+  isPressed = false;
 });
-canvas.addEventListener('touchmove', e => {
-	if (isPressed) {
-		calculateMousePosition(e.touches[0]);
-	}
+canvas.addEventListener("touchmove", e => {
+  if (isPressed) {
+    calculateMousePosition(e.touches[0]);
+  }
 });
 
 draw();
